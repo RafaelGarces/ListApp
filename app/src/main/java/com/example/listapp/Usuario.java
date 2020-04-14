a9dc1fdb77e2381cd670cb92df15ab41c4f2ee4d
rafa <garceshermosillarafael@gmail.com>
2020-04-14 19:26:43 +0200

    hacer el pantalla principal

---
+++ app/src/main/java/com/example/listapp/Usuario.java
@@ -0,0 +1,38 @@
+package com.example.listapp;
+
+import java.io.Serializable;
+
+public class Usuario implements Serializable {
+    int id;
+    String nombre, password;
+
+    public Usuario(int id, String nombre, String password) {
+        this.id = id;
+        this.nombre = nombre;
+        this.password = password;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
